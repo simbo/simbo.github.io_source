9e28501cb016a54621a8ce981ce07d81e6d6d6c3
Simon Lepel <simbo@simbo.de>
2018-05-24 22:14:11 +0200

    add cname file for custom domain and webpack copy task

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const autoprefixer = require('autoprefixer');
 const chalk = require('chalk');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const cssMqpacker = require('css-mqpacker');
 const cssnano = require('cssnano');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -203,6 +204,12 @@ const config = {
     new CleanWebpackPlugin(['dist'], {
       watch: true
     }),
+
+    new CopyWebpackPlugin([{
+      from: rootPath('src/public/**/*'),
+      to: rootPath('dist'),
+      context: rootPath('src/public')
+    }]),
 
     new webpack.optimize.OccurrenceOrderPlugin(),
 
