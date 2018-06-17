5a637af1230ff2a17bf3115e85ca379aff2503cc
Simon Lepel <simbo@simbo.de>
2018-06-17 22:39:22 +0200

    add sidebar component

--- src/scripts/components/index.ts
+++ src/scripts/components/index.ts
@@ -1,5 +1,7 @@
 import TypedComponent from './typed/typed.component.vue'
+import SidebarComponent from './sidebar/sidebar.component.vue'
 
 export const components = {
-  'typed': TypedComponent
+  'typed': TypedComponent,
+  'sidebar': SidebarComponent,
 };
---
+++ src/scripts/components/sidebar/sidebar.component.vue
@@ -0,0 +1,3 @@
+<template lang="pug" src="./sidebar.component.pug"></template>
+<style lang="scss" src="./sidebar.component.scss"></style>
+<script lang="ts" src="./sidebar.component.ts"></script>
